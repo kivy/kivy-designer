a3312dd41b0c7eb43d7c1fdadb7c894da2d9f8e6
Abhinav <abhijangda@hotmail.com>
2013-06-11 00:30:09 +0530

    Added events related to different ActionItemButtons in EditContView

--- designer/uix/editcontview.py
+++ designer/uix/editcontview.py
@@ -3,6 +3,28 @@ from kivy.uix.actionbar import ContextualActionView
 
 class EditContView(ContextualActionView):
 
-    def __init__(self, **kwargs):
-        super(ContextualActionView, self).__init__(**kwargs)
+    __events__ = ('on_undo', 'on_redo', 'on_cut', 'on_copy', 'on_paste',
+                  'on_delete', 'on_selectall',)
+    
+    def on_undo(self, *args):
+        pass
+
+    def on_redo(self, *args):
+        pass
+
+    def on_cut(self, *args):
+        pass
+
+    def on_copy(self, *args):
+        pass
+
+    def on_paste(self, *args):
+        pass
+
+    def on_delete(self, *args):
+        pass
+
+    def on_selectall(self, *args):
+        pass
+    
     
