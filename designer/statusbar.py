d8e13aff98781b6aa9d09c8a03ce1592b1c8b5ae
Abhinav <abhijangda@hotmail.com>
2013-06-24 19:10:23 +0530

    Added Statusbar.show_message

--- designer/statusbar.py
+++ designer/statusbar.py
@@ -12,12 +12,22 @@ class StatusNavBarSeparator(Label):
 class StatusBar(BoxLayout):
     app = ObjectProperty()
     navbar = ObjectProperty()
+    gridlayout = ObjectProperty()
+    
+    def show_message(self, message):
+        self.app.widget_focused = None
+        self.clear_widgets()
+        label = Label(text=message)
+        self.add_widget(label)
 
     def on_app(self, instance, app):
         app.bind(widget_focused=self.update_navbar)
 
     def update_navbar(self, *largs):
+        self.clear_widgets()
+        self.add_widget(self.gridlayout)
         self.navbar.clear_widgets()
+
         wid = self.app.widget_focused
         if not wid:
             return
