32c283948fd7da7af3186ed82d6fd31d4a0ccb56
Aron Bordin <aron.bordin@gmail.com>
2015-05-30 00:16:36 -0300

    bug reporter

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+import traceback
+from kivy.core.window import Window
+from designer.app import DesignerApp
+from designer.uix.bug_reporter import BugReporterApp
+
 if __name__ == '__main__':
-    from designer.app import DesignerApp
-    DesignerApp().run()
+    try:
+        DesignerApp().run()
+    except Exception as e:
+        for child in Window.children:
+            Window.remove_widget(child)
+        BugReporterApp(traceback.format_exc()).run()
