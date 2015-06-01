c4cc6052b36b12221f45447591c8ea0b6fc4e376
Aron Bordin <aron.bordin@gmail.com>
2015-06-01 01:12:46 -0300

    python console autocomplete

--- designer/uix/py_console.py
+++ designer/uix/py_console.py
@@ -7,7 +7,7 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.base import runTouchApp
 from kivy.clock import Clock
 from kivy.properties import ObjectProperty, ListProperty,\
-    StringProperty, NumericProperty
+    StringProperty, NumericProperty, partial
 from kivy.lang import Builder
 
 Builder.load_string('''
@@ -97,7 +97,7 @@ class Shell(code.InteractiveConsole):
             exec(_code, self.locals)
         except SystemExit:
             print('It\'s not possible to exit from Kivy Designer'
-                                                    'Python console')
+                                                    ' Python console')
         except:
             self.showtraceback()
 
@@ -148,7 +148,7 @@ class Shell(code.InteractiveConsole):
                         continue
                     # Can be None if sys.stdin was redefined
                     encoding = getattr(sys.stdin, "encoding", None)
-                    if encoding and not isinstance(line, str):
+                    if encoding and isinstance(line, bytes):
                         line = line.decode(encoding)
                 except EOFError:
                     self.write("\n")
@@ -190,7 +190,25 @@ class InteractiveShellInput(TextInput):
     def keyboard_on_key_down(self, window, keycode, text, modifiers):
         '''Override of _keyboard_on_key_down.
         '''
-        if keycode[0] == 13:
+        if keycode[0] == 9:
+            # tab, check if completer is available
+            try:
+                from rlcompleter import Completer
+                txt = self.text[self._cursor_pos:]
+
+                if txt.strip():
+                    suggestion = Completer(self.sh.locals).complete(txt, 0)
+                    if suggestion:
+                        self.select_text(self._cursor_pos,
+                                              self._cursor_pos + len(txt))
+                        self.delete_selection()
+                        Clock.schedule_once(
+                            partial(self.insert_text, suggestion))
+                    return False
+            except ImportError:
+                pass
+
+        elif keycode[0] == 13:
             # For enter
             self.last_line = self.text[self._cursor_pos:]
             self.dispatch('on_ready_to_input')
@@ -278,7 +296,8 @@ class PythonConsole(BoxLayout):
         self._thread = InteractiveThread(self.sh)
         self._thread.setDaemon(True)
 
-        Clock.schedule_once(self.run_sh, 0)
+        Clock.schedule_once(self.run_sh)
+        self.text_input.sh = self.sh
         self._ready_to_input = False
         self._exit = False
 
