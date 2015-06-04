39a846990eff0e72f23de0d24bc72a64a973c1d5
Aron Bordin <aron.bordin@gmail.com>
2015-06-04 13:09:14 -0300

    basic spec editor ok

--- designer/buildozer_spec_editor.py
+++ designer/buildozer_spec_editor.py
@@ -1,10 +1,18 @@
 import os
 import json
+from kivy.core.window import Window
+from kivy.metrics import dp
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.popup import Popup
+from kivy.uix.togglebutton import ToggleButton
+from kivy.uix.widget import Widget
+import webbrowser
 import designer
 
-from kivy.properties import ObjectProperty, ConfigParser
+from kivy.properties import ObjectProperty, ConfigParser, DictProperty
 from kivy.uix.settings import Settings, InterfaceWithSidebar, MenuSidebar,\
-    ContentPanel, SettingsPanel
+    ContentPanel, SettingsPanel, SettingOptions, SettingItem, SettingSpacer
 
 
 class SpecContentPanel(ContentPanel):
@@ -16,7 +24,69 @@ class SpecMenuSidebar(MenuSidebar):
 
 
 class SpecEditorInterface(InterfaceWithSidebar):
-    pass
+
+    def open_buildozer_docs(self, *args):
+        webbrowser.open('http://buildozer.readthedocs.org')
+
+
+class SpecSettingDict(SettingItem):
+    '''Implementation of an option list on top of a :class:`SettingItem`.
+    Based on SettingOptions, but implemented to use DictProperty.
+    It is visualized with a :class:`~kivy.uix.label.Label` widget that, when
+    clicked, will open a :class:`~kivy.uix.popup.Popup` with a
+    list of options from which the user can select.
+    '''
+
+    options = DictProperty({})
+    '''Dict with keys to be saved and visible values to the user
+
+    :attr:`options` is a :class:`~kivy.properties.DictProperty` and defaults
+    to [].
+    '''
+
+    popup = ObjectProperty(None, allownone=True)
+    '''(internal) Used to store the current popup when it is shown.
+
+    :attr:`popup` is an :class:`~kivy.properties.ObjectProperty` and defaults
+    to None.
+    '''
+
+    def on_panel(self, instance, value):
+        if value is None:
+            return
+        self.bind(on_release=self._create_popup)
+
+    def _set_option(self, instance):
+        self.value = instance.key
+        self.popup.dismiss()
+
+    def _create_popup(self, instance):
+        # create the popup
+        content = BoxLayout(orientation='vertical', spacing='5dp')
+        popup_width = min(0.95 * Window.width, dp(500))
+        self.popup = popup = Popup(
+            content=content, title=self.title, size_hint=(None, None),
+            size=(popup_width, '400dp'))
+        popup.height = len(self.options) * dp(55) + dp(150)
+
+        # add all the options
+        content.add_widget(Widget(size_hint_y=None, height=1))
+        uid = str(self.uid)
+        for key in self.options:
+            state = 'down' if key == self.value else 'normal'
+            btn = ToggleButton(text=self.options[key], state=state, group=uid)
+            btn.key = key
+            btn.bind(on_release=self._set_option)
+            content.add_widget(btn)
+
+        # finally, add a cancel button to return on the previous panel
+        content.add_widget(SettingSpacer())
+        btn = Button(text='Cancel', size_hint_y=None, height=dp(50))
+        btn.bind(on_release=popup.dismiss)
+        content.add_widget(btn)
+
+        # and open the popup !
+        popup.open()
 
 
 class SpecSettingsPanel(SettingsPanel):
@@ -37,6 +107,11 @@ class SpecSettingsPanel(SettingsPanel):
         else:
             return ''
 
+    def set_value(self, section, key, value):
+        if not value and not self.config.has_option(section, key):
+            return False
+        super(SpecSettingsPanel, self).set_value(section, key, value)
+
 
 class BuildozerSpecEditor(Settings):
     '''Subclass of :class:`kivy.uix.settings.Settings` responsible for
@@ -50,6 +125,7 @@ class BuildozerSpecEditor(Settings):
 
     def __init__(self, **kwargs):
         super(BuildozerSpecEditor, self).__init__(**kwargs)
+        self.register_type('dict', SpecSettingDict)
 
     def load_settings(self, proj_dir):
         '''This function loads project settings
@@ -77,12 +153,6 @@ class BuildozerSpecEditor(Settings):
         self.add_json_panel('Buildozer', self.config_parser,
                             os.path.join(_dir, 'designer',
                             'settings', 'buildozer_spec_buildozer.json'))
-
-    def on_config_change(self, *args):
-        '''This function is default handler of on_config_change event.
-        '''
-        self.config_parser.write()
-        super(BuildozerSpecEditor, self).on_config_change(*args)
 
     def create_json_panel(self, title, config, filename=None, data=None):
         '''Override the original method to use the custom SpecSettingsPanel
@@ -121,5 +191,3 @@ class BuildozerSpecEditor(Settings):
             panel.add_widget(instance)
 
         return panel
-
-    #TODO on write check if value is '' and not exists, so ignore
