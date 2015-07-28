a221bedcc1eb92e70a82fa4bffede9ad96a4b6fe
Aron Bordin <aron.bordin@gmail.com>
2015-07-27 23:09:19 -0300

    actionbutton desc

--- designer/uix/designer_action_items.py
+++ designer/uix/designer_action_items.py
@@ -1,6 +1,9 @@
 from kivy.metrics import sp
 from kivy.properties import ObjectProperty, StringProperty
-from kivy.uix.actionbar import ActionGroup, ActionPrevious, ActionButton
+from kivy.uix.actionbar import ActionGroup, ActionPrevious, ActionButton, \
+    ActionItem
+from kivy.uix.behaviors import ButtonBehavior
+from kivy.uix.floatlayout import FloatLayout
 from designer.uix.actioncheckbutton import ActionCheckButton
 from designer.uix.contextual import ContextSubMenu
 
@@ -36,18 +39,28 @@ class DesignerActionGroup(ActionGroup):
     pass
 
 
-class DesignerActionButton(ActionButton):
+class DesignerActionButton(ActionItem, ButtonBehavior, FloatLayout):
     '''DesignerActionButton is a ActionButton to the ActionBar menu
     '''
 
+    text = StringProperty('Button')
+    '''text which is displayed in the DesignerActionButton.
+       :data:`text` is a :class:`~kivy.properties.StringProperty` and defaults
+       to 'Button'
+    '''
+
+    desc = StringProperty('')
+    '''text which is displayed as description to DesignerActionButton.
+       :data:`desc` is a :class:`~kivy.properties.StringProperty` and defaults
+       to ''
+    '''
     cont_menu = ObjectProperty(None)
 
     def __init__(self, **kwargs):
         super(DesignerActionButton, self).__init__(**kwargs)
         self.minimum_width = 200
-        self.on_press = self.on_btn_press
 
-    def on_btn_press(self, *args):
+    def on_press(self, *args):
         '''
         Event to hide the ContextualMenu when a ActionButton is pressed
         '''
