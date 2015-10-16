0a93e1d5355802746a513e7e64625f18fe9b3ec7
Aron Bordin <aron.bordin@gmail.com>
2015-10-16 02:26:45 -0300

    remove custom CodeInput._get_bbcode

--- designer/uix/designer_code_input.py
+++ designer/uix/designer_code_input.py
@@ -44,27 +44,6 @@ class DesignerCodeInput(CodeInput):
         self.background_color = get_color_from_hex(self.style.background_color)
         self._trigger_refresh_text()
 
-    def _get_bbcode(self, ntext):
-        # override the default method to fix bug with custom themes
-        # get bbcoded text for python
-        try:
-            ntext[0]
-            # replace brackets with special chars that aren't highlighted
-            # by pygment. can't use &bl; ... cause & is highlighted
-            ntext = ntext.replace(u'[', u'\x01').replace(u']', u'\x02')
-            ntext = highlight(ntext, self.lexer, self.formatter)
-            ntext = ntext.replace(u'\x01', u'&bl;').replace(u'\x02', u'&br;')
-            # replace special chars with &bl; and &br;
-            ntext = ''.join((u'[color=', str(self.text_color), u']',
-                             ntext, u'[/color]'))
-            ntext = ntext.replace(u'\n', u'')
-            # remove possible extra highlight options
-            ntext = ntext.replace(u'[u]', '').replace(u'[/u]', '')
-
-            return ntext
-        except IndexError:
-            return ''
-
     def on_show_edit(self, *args):
         pass
 
