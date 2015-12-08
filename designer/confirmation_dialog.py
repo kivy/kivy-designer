e97cb202d4f14c74f6b69ccb4b16ad3fa350943c
kiok46 <kuldeepbb.grewal@gmail.com>
2015-12-08 14:45:57 +0530

    adding a confirmationdialog, 2 methods and other fixes

--- designer/confirmation_dialog.py
+++ designer/confirmation_dialog.py
@@ -27,3 +27,32 @@ class ConfirmationDialog(BoxLayout):
 
     def on_cancel(self, *args):
         pass
+
+
+class ConfirmationDialogSave(BoxLayout):
+    '''ConfirmationDialogSave shows a confirmation message with three buttons
+       "Save", "Don't Save" and "Cancel". It may be used for confirming user
+       about an operation. It emits 'on_save' when "Save" is pressed,
+       'on_dont_save' when "Don't Save" is pressed and 'on_cancel'
+       when "No" is pressed.
+    '''
+
+    message = StringProperty('')
+    '''It is the message to be shown
+       :data:`message` is a :class:`~kivy.properties.StringProperty`
+    '''
+
+    __events__ = ('on_save', 'on_dont_save', 'on_cancel')
+
+    def __init__(self, message):
+        super(ConfirmationDialogSave, self).__init__()
+        self.message = message
+
+    def on_save(self, *args):
+        pass
+
+    def on_dont_save(self, *args):
+        pass
+
+    def on_cancel(self, *args):
+        pass
